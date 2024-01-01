ca551aa3e215f7bf8f63f6ba5fe5fc772b90f978
[Ayush Goyal] <[itsayushgoyal1003@gmail.com]>
2024-01-01 15:40:53 +0530

    Solved Problem 1368A

---
+++ 800 Rated Problems/1368A - C+=/1368A_C+=.cpp
@@ -0,0 +1,33 @@
+// Problem 1368A - C+=
+// Solved On 01/01/2024
+// https://codeforces.com/problemset/problem/1368/A
+// Explanation given after the code
+
+#include <bits/stdc++.h>
+
+int main(){
+    int t;
+    std::cin >> t;
+    while (t--){
+        int a, b, n;
+        std::cin >> a >> b >> n;
+        int count = 0;
+        while (a <= n && b <= n){
+            if (a <= b){
+                a += b;
+                count++;
+            }
+            else if (a > n){
+                break;
+            }
+            else if (b <= a){
+                b += a;
+                count++;
+            }
+            else if (b > n){
+                break;
+            }
+        }
+        std::cout << count << std::endl;
+    }
+}
